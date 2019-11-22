24ff6034c44e558038a7f8808be37eadf4bd8083
shaggieization <galoop8@gmail.com>
2019-11-22 10:25:19 -0600

    REVISION

---
+++ LUAN/Project_LUAN/Assets/escenamen.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class escenamen : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public void escena2()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+}
